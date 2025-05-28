8617166de5fc5630c475b0ec89ba701be0a329f5
Haris Javed <harisj58@gmail.com>
2025-05-28 19:06:50 +0000

    chore, chore: amend tool output, define new tool
    - amend output of `get_matching_locations` tool
    - define `get_cuisine_by_area` to lookup cuisines in an area

--- ChatbotFunctions.py
+++ ChatbotFunctions.py
@@ -24,6 +24,20 @@ class ChatbotFunctions:
                 "required": ["area"],
             },
         },
+        "get_cuisine_by_area": {
+            "name": "get_cuisine_by_area",
+            "description": "Get the types of cuisine available at a particular area in Bengaluru. Use this function to develop recommendations for a user looking to dine at a spot in Bengaluru. Make sure the area you are looking for is exactly the same as the one you get from `get_matching_locations`.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "area": {
+                        "type": "string",
+                        "description": "The area in Bengaluru the user wants to lookup the FoodieSpot joint in. e.g.: 'Koramangala', 'Whitefield' etc.",
+                    }
+                },
+                "required": ["area"],
+            },
+        },
     }
 
     __restaurants_data = json.load(open("./data/restaurants_data.json", "r"))
@@ -53,9 +67,30 @@ class ChatbotFunctions:
         results = []
         for matched_area, score, idx in top_matches:
             place = area_place_pairs[idx][1]
-            results.append({"match_score": score, "place": place["location"]["area"]})
+            results.append(place["location"]["area"])
 
-        return f"Here is a list of the matching areas FoodieSpot is located in: {str(results)}\n\nUse these to determine if the location user has requested is available or not. If not, suggest alteratives from the list otherwise proceed as normal."
+        return f"Here is a list of the matching areas FoodieSpot is located in: {str(results)}\n\nList these options in a proper numbered fashion to the user and ask him to pick a number as a confirmation. Then, proceed with that area unless the user asks to search for another location."
+
+    @classmethod
+    def get_cuisine_by_area(cls, area: str) -> str:
+        """
+        Returns a list of unique cuisines found in the given area.
+
+        Args:
+            area (str): The name of the area/city to filter restaurants.
+
+        Returns:
+            str: A message containing unique cuisines found in the given location.
+        """
+        cuisines = set()
+
+        for restaurant in cls.__restaurants_data:
+            rest_location = restaurant.get("location", {}).get("area", "").lower()
+            if area.lower() == rest_location:
+                cuisine_field = restaurant.get("cuisine", [])
+                cuisines.update(cuisine_field)
+
+        return f"Here is a list of cuisines available at location {area}: {sorted(cuisines)}\n\nUse this list to show the various cuisine options available in an area. If the list is empty, ask the user to pick some other area."
 
     @classmethod
     def get_descriptions(cls):
